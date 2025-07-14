d47b4d400d54cb3f61d76006aa4a59c478ae7d6e
Thiago Pacheco <thiagodelimapacheco@gmail.com>
2025-07-13 20:11:23 -0400

    dotfiles update

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -1,7 +1,19 @@
 eval (/opt/homebrew/bin/brew shellenv)
 starship init fish | source
-set -x NVM_DIR $HOME/.nvm
 zoxide init fish | source
 alias cd="z"
 alias vim="nvim"
 alias tm="tmuxinator"
+set -gx EDITOR nvim
+set -gx WORKDIR /Users/pacheco/dev
+set -gx PATH "$HOME/.local/bin" $PATH
+set -gx NGROK_TOKEN 2q1nV9EJe7jiPZRvoGgUSJnAZJy_7AdTfUxBywqv9uX9wHtvU
+set -gx PATH /opt/homebrew/opt/postgresql@16/bin $PATH
+fish_add_path /opt/homebrew/opt/postgresql@16/bin
+
+# make node available by default with nvm use 18
+nvm use 20
+alias lg='lazygit'
+
+# opencode
+fish_add_path /Users/pacheco/.opencode/bin
